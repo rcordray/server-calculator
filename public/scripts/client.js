2e4ce6bec16fc9512b079684b77687ba454d6dad
Robert Cordray <robertcordray@gmail.com>
2017-08-07 10:08:15 -0500

    add a form on the client with two inputs and a button

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
     console.log('jquery sourced');
 
+    $('#addButton').on('click', function() {
+        console.log('Add button click');
+        console.log($('#firstInput').val());
+        console.log($('#secondInput').val());
+
+    })
 })
