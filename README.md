a3befb67f458faf1e1fc70e32898773ecda793c4
Robert Cordray <robertcordray@gmail.com>
2017-08-07 10:28:05 -0500

    create AJAX request

--- README.md
+++ README.md
@@ -6,10 +6,14 @@ does math calculations on the server.
 ## Task List
 - [x] Static files on locallhost
 - [x] Add jQUery and source client.js
-- [ ] add a form on the client with two inputs and a button
+- [x] add a form on the client with two inputs and a button
 - [ ] create AJAX request
 - [ ] add a route for calculate in the app.js
 - [ ] get results back to client
 - [ ] display on the DOM
 - [ ] add remaining operators
 - [ ] clear button
+
+
+## Stretch Goal
+- [ ] 
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,6 +5,20 @@ $(document).ready(function() {
         console.log('Add button click');
         console.log($('#firstInput').val());
         console.log($('#secondInput').val());
+        var firstNumber = $('#firstInput').val();
+        var secondNumber = $('#secondInput').val();
+        doCalculation(firstNumber, secondNumber, 'add');
+    });
+});
+// opp is the operator
+function doCalculation(x, y, opp) {
+    $.ajax({
+        method: 'POST',
+        url: '/calculator',
+        data: { x: x, y: y, opp: opp },
+        success: function(response) {
+            console.log(response);
 
-    })
-})
+        }
+    });
+}
